f79e3a99cbb97453fcae665e4f6b29c33b7e243e
Andrei <nicaandrei00@gmail.com>
2024-03-10 16:52:08 +0200

    Update for sending and receiving data from front end

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const app = express()
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
+// nodemon server
+// postman
+
+var users = [];
+
+app.get("/api/", (req, res) => {
+    res.json(JSON.stringify(users))
+    console.log("SENT")
+})
+
+function addNewUser(userData)
+{
+    users.push(userData);
+    console.log(users);
+}
+
+app.post("/api/newuser", (req, res) => {
+    const newUserData = req.body;
+    console.log(newUserData);
+    addNewUser(newUserData);
 })
 
 const port = 4987
