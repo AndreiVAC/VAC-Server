444aa00ccbb71cda19fd233a4c0a7b939e858eaf
Andrei <nicaandrei00@gmail.com>
2024-03-17 20:48:51 +0200

    Login check

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ app.use(express.urlencoded({ extended: true }))
 
 var users = require('./data.json');
 
-
 app.get("/api/", (req, res) => {
-    res.json(JSON.stringify(users))///cu ce vrei sa trimiti 
+    res.json(JSON.stringify(users))
     console.log("SENT")
 })
 
@@ -25,13 +24,36 @@ function addNewUser(userData)
     })
     console.log(users);
 }
-///GET POST
+
 app.post("/api/newuser", (req, res) => {
     let newUserData = req.body;
     console.log(newUserData);
     addNewUser(newUserData);
-    res.send("trimis");
+    res.send(true);
 })
 
-const port = 4987
+function tryLogin(data){
+    for(let i=0; i<users.length; i++)
+    {
+        console.log(users[i]['mail']);
+        if(users[i]['mail'] === data['mail']){
+            if(users[i]['pass'] === data['pass']){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+app.post("/api/login", (req, res) => {
+    let loginInfo = req.body;
+    if (tryLogin(loginInfo)){
+        res.send(true);
+    }
+    else{
+        res.send(false);
+    }
+})
+
+const port = 8080
 app.listen(port, () => {console.log(`Server started on port ${port}.`)})
