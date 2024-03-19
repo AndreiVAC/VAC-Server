8046f4413fa9ac3fffe58fac797123ff14a6e5b3
Andrei <nicaandrei00@gmail.com>
2024-03-19 19:38:58 +0200

    Check for user already exists when signing up

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(function (req, res, next) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://atc-2024-valve-anti-cheat-fe-linux-web-app.azurewebsites.net');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // https://atc-2024-valve-anti-cheat-fe-linux-web-app.azurewebsites.net
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
@@ -21,7 +21,7 @@ app.use(function (req, res, next) {
 
 var users = require('./data.json');
 
-app.get("/api/", (req, res) => {
+app.get("/users", (req, res) => {
     res.json(JSON.stringify(users))
     console.log("SENT")
 })
@@ -37,11 +37,55 @@ function addNewUser(userData)
     console.log(users);
 }
 
+function verifyData(data)
+{
+    for(let i=0; i<users.length; i++)
+    {
+        if(users[i]['mail'] === data['mail']){
+            return false;
+        }
+        if(users[i]['name'] === data['name']){
+            return false;
+        }
+    }
+    let re = /^[a-zA-Z0-9._-]{5,}$/;
+    if (re.test(data.name))
+    {
+        re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/
+        if(re.test(data.pass))
+        {
+            re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            if(re.test(data.mail))
+            {
+                if ('organization' in data){
+                re = /^\w[\w\d\s.,#-]{4,58}\w$/
+                if (re.test(data.organization))
+                {
+                    if (re.test(data.headquarters))
+                    {
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                return true;
+            }
+            }
+        }
+    }
+    return false
+}
+
 app.post("/newuser", (req, res) => {
     let newUserData = req.body;
-    console.log(newUserData);
-    addNewUser(newUserData);
-    res.send(true);
+    if (verifyData(newUserData)){
+        addNewUser(newUserData);
+        res.send(true);
+    }
+    else{
+        res.send(false);
+    }
 })
 
 function tryLogin(data){
